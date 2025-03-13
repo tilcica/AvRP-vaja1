1f47ecb57a43043175e0c07ebb1c0fa284076cbd
Tilen Gašparič <tilen623@gmail.com>
2025-03-13 20:43:58 +0100

    added most of the functionality of Roman Sort. debugging left to do

--- vaja 1/main.cpp
+++ vaja 1/main.cpp
@@ -1,18 +1,52 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 string romanSort(string inputText) {
-	return(inputText);
+	vector<int> numbers;
+	string output = "";
+	istringstream iss(inputText);
+	int num;
+	while (iss >> num) {
+		numbers.push_back(num);
+	}
+	int minNum = *min_element(numbers.begin(), numbers.end());
+	int maxNum = *max_element(numbers.begin(), numbers.end());
+	vector<int> B(maxNum-minNum + 1, 0);
+	for (int i : numbers) {
+		i -= minNum;
+		B[i]++;
+	}
+	int counter = 0;
+	for (int i : B) {
+		if (i != 0) {
+			for (int j = 0; j < i; j++) {
+				output += i + " ";
+			}
+		}
+	}
+	
+	return(output);
 }
 string countingSort(string inputText) {
 	return(inputText);
 }
 
 int main(int argc, char* argv[]) {
-	string sortType = argv[1];
-	string inputFile = argv[2];
+	string sortType, inputFile;
+	if (argc >= 3) {
+		sortType = argv[1];
+		inputFile = argv[2];
+	}
+	else {
+		sortType = "1";
+		inputFile = "C:/Users/Tilen/Desktop/sula naloge/4 semester/algoritmi v racunalniski praksi/vaja 1/log.txt";
+	}
+
 
 	fstream file;
 	string input = "";
@@ -32,6 +66,7 @@ int main(int argc, char* argv[]) {
 	}
 	else if (sortType == "1") {
 		output = "Roman sort: \n" + romanSort(input);
+		cout << output;
 	}
 	else {
 		cout << "invalid sort type. valid ones are 0 (Counting) and 1 (Roman)" << endl;
